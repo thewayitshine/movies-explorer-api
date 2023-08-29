3a038f159919241f1335d1c49586d3e8239b83ca
Elizaveta Petrenko <mistress.camerawork@yandex.ru>
2023-08-29 21:23:39 +0300

    fix word mistakes

--- utils/config.js
+++ utils/config.js
@@ -4,7 +4,7 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 
 const config = {
   jwtSecret: NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret-key',
-  bitfilmsdb: 'mongodb://0.0.0.0:27017/bitfilmsdb',
+  bitfilmsdb: 'mongodb://localhost:27017/bitfilmsdb',
 };
 
 module.exports = config;
